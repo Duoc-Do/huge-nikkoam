35bbdff533aa8ab342a2fbe12f64bc4f866f5f64
DuocDo <duocdo.it@gmail.com>
2021-09-23 04:28:12 +0700

    updated

--- src/js/main.js
+++ src/js/main.js
@@ -7,28 +7,26 @@ var carousel = new bootstrap.Carousel(myCarousel, {
 
 /* ====== GSAP ======= */
 gsap.registerPlugin(ScrollTrigger);
-//WITH Timelines (cleaner, more versatile)
-var tl = gsap.timeline({ duration: 0.5 });
-tl.from(".wave-oval svg path", {
-  scale: 0.96,
-  yoyo: true,
-  repeat: -1,
-  ease: Power1.easeInOut,
-});
+var tl = gsap.timeline({ opacity: 1 });
 
-// wave effect
 tl.to(".wave-shape svg path", {
-  duration: 5,
   opacity: 0.2,
-  y: 120,
-  x: 40,
+  duration: 3,
+  y: 100,
+  x: 30,
   ease: "Power1.easeInOut",
   stagger: {
     grid: "auto",
     from: "end",
     axis: "y",
     amount: 1.5,
-    yoyou: true,
+    yoyo: true,
     repeat: -1,
   },
+});
+tl.from(".wave-oval svg path", {
+  scale: 0.96,
+  yoyo: true,
+  repeat: -1,
+  ease: "Power1.easeInOut",
 });
